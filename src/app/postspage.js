365883fcb3dc5a3550f1557ee8d6774f8ff5f905
Paruyr Tsaturyan <paruyr.tsaturyan@softconstruct.com>
2023-12-07 12:08:16 +0400

    changed toml file

---
+++ src/app/postspage.js
@@ -0,0 +1,30 @@
+// at the top of your component file
+import { client } from "@utils/prismicPosts";
+import Post from "@components/Post";
+
+// inside your component markup, pull `posts` from props
+<div className="posts">
+  {posts !== undefined &&
+    posts.map((p) => {
+      let title = p.title[0].text;
+      let key = `${p.date}+${title}`;
+      return <Post key={key} date={p.date} image={p.image} title={title} />;
+    })}
+</div>;
+
+// at the bottom of your component file
+export async function getStaticProps() {
+  // query() is empty on purpose!
+  // https://prismic.io/docs/rest-api/query-the-api/query-all-documents
+  const res = await client.query("");
+
+  const posts = res.results.map((p) => {
+    return p.data;
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
--- src/prismicPosts.js
+++ src/prismicPosts.js
@@ -1,7 +1,7 @@
-import Prismic from 'prismic-javascript'
+import Prismic from "prismic-javascript";
 
-const REPOSITORY = Catalyst_prismicio /* your repo name */
-export const API_URL = `https://${REPOSITORY}.cdn.prismic.io/api/v2`
-export const API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN
+const REPOSITORY = process.env.NEXT_PUBLIC_PRISMIC_REPO_NAME; /* repo name */
+export const API_URL = `https://${REPOSITORY}.cdn.prismic.io/api/v2`;
+export const API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN;
 
-export const client = Prismic.client(API_URL, { accessToken: API_TOKEN })
+export const client = Prismic.client(API_URL, { accessToken: API_TOKEN });
