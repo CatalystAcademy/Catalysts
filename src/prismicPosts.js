3b3c06c64dd0097199cd6341c598f0701570b040
Paruyr Tsaturyan <paruyr.tsaturyan@softconstruct.com>
2023-12-07 12:44:03 +0400

    modified toml file

--- src/prismicPosts.js
+++ src/prismicPosts.js
@@ -1,7 +1,6 @@
 import Prismic from "prismic-javascript";
 
-const REPOSITORY = process.env.NEXT_PUBLIC_PRISMIC_REPO_NAME; /* repo name */
-export const API_URL = `https://${REPOSITORY}.cdn.prismic.io/api/v2`;
+export const API_URL = process.env.PRISMIC_API_ENDPOINT;
 export const API_TOKEN = process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN;
 
 export const client = Prismic.client(API_URL, { accessToken: API_TOKEN });
